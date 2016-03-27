c72f539b18cb1ba09be1dc2e86d52e1c33de2990
Tom McCann <tom@mymato.com>
2016-03-27 17:28:37 +1300

    Fix to bug 1234

--- MyThirdAttempt/src/Front.java
+++ MyThirdAttempt/src/Front.java
@@ -3,7 +3,7 @@ public class Front {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git User");
+		System.out.println("Hello Git User - this fixes bug #1234");
 	}
 
 }
